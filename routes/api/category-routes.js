73d5c4bf1a229f4a9067c91111d78109c5fe3b07
arnald18 <acenaldy04@gmail.com>
2023-08-08 19:08:09 -0400

    add GET /api/categories/:id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,9 +17,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    // find one category by its `id` value
+    const category = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    // RETURN A 404 ERROR IF THE CATEGORY WAS NOT FOUND
+    if (!category) {
+      return res
+        .status(404)
+        .json({ message: "Could not find category with that id" });
+    }
+    // RETURN THE CATEGORY SUCCESSFULLY
+    return res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
