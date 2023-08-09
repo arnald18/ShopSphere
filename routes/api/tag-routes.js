4b24c417bb7f5648dd6aa07ca353a3f72fff7d6f
arnald18 <acenaldy04@gmail.com>
2023-08-08 22:18:55 -0400

    add PUT in tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,10 +45,28 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tag) => {
+      res.status(200).json(tag);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
